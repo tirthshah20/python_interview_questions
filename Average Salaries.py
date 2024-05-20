b599864bf5cc71fd4caa5f9354ce63df152ebe12
Tirth Shah <63912314+tirthshah20@users.noreply.github.com>
2024-05-20 22:41:49 +0530

    Create Average Salaries.py

---
+++ Average Salaries.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+# Start writing code
+employee.head()
+
+emp1=employee.groupby(['department'])['salary'].mean().reset_index()
+emp1.rename(columns={'salary':'Avg_salary'},inplace=True)
+
+result=pd.merge(employee,emp1,on='department',how='left')
+result[['first_name','department','salary','Avg_salary']]
